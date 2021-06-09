b1b4558c74bb860b360f33aec1e8b4e05fa7043d
DESKTOP-1T5ODT0\USER <jacktoaburaage@gmail.com>
2021-06-09 13:35:38 +0900

    衝突判定実装

---
+++ MultiplayerVRoom/Assets/IRONHEAD Games/Scripts/DartsBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartsBall : MonoBehaviour
+{
+
+    private Collider coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+        
+    }
+}
