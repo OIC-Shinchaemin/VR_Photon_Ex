213ef314fb6f8c729e66f8bc9f0a60dad6a20d41
OiC-b8073-Ochi <b8073@oic.jp>
2021-06-22 17:52:33 +0900

    (1)おっきいtrigger追加(2)体のカプセルコライダー縮小

---
+++ MultiplayerVRoom/Assets/UtasakiBox/Script/TennisRacket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TennisRacket : MonoBehaviour
+{
+    public GameObject Ball;
+    private float speedZ = 1.8f;
+    private float speedY = 0.6f;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Ball")
+        {
+            Vector3 Force;
+            Force = new Vector3(0, speedY, speedZ);
+            Ball.GetComponent<Rigidbody>().AddForce(Force, ForceMode.Impulse);
+        }
+    }
+}
