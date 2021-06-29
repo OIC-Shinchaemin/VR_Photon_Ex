5e30c6c867f5049d379513dca1573ae0dae114e7
OiC-b8073-Ochi <b8073@oic.jp>
2021-06-29 09:36:24 +0900

    すり抜け問題解決しません

--- MultiplayerVRoom/Assets/UtasakiBox/Script/TennisRacket.cs
+++ MultiplayerVRoom/Assets/UtasakiBox/Script/TennisRacket.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class TennisRacket : MonoBehaviour
 {
-    public GameObject Ball;
-    private float speedZ = 1.8f;
-    private float speedY = 0.6f;
+    public GameObject ball;
+
+    private Rigidbody rb;
+
+    private float power = 3.0f;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Ball")
         {
             Vector3 Force;
-            Force = new Vector3(0, speedY, speedZ);
-            Ball.GetComponent<Rigidbody>().AddForce(Force, ForceMode.Impulse);
+            Force = rb.velocity * power;
+            Debug.Log(Force);
+            ball.GetComponent<Rigidbody>().AddForce(Force, ForceMode.Impulse);
         }
     }
 }
